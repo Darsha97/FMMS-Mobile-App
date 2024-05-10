578a264416751df79a3af042b4922da427f67035
KavindaSandamal <info.kavindasandamal@gmail.com>
2024-05-11 00:27:38 +0530

    update UI

--- lib/mongodb.dart
+++ lib/mongodb.dart
@@ -25,8 +25,7 @@ class MongoDatabase {
     }
   }
 
-  // Function to handle user login
-  static Future<bool> login(String username, String password) async {
+  static Future<String?> login(String username, String password) async {
     try {
       // Ensure that the database connection is established
       if (db == null) {
@@ -36,10 +35,18 @@ class MongoDatabase {
       var userCollection = db!.collection(userCollectionName);
       var user = await userCollection
           .findOne({'regNo': username, 'password': password});
-      return user != null;
+
+      if (user != null) {
+        // Return the userId if the user exists
+        return user['_id']
+            .toString(); // Assuming '_id' is the field containing the userId
+      } else {
+        // Return null if the user does not exist or the credentials are incorrect
+        return null;
+      }
     } catch (e) {
       log(e.toString());
-      return false;
+      return null; // Return null in case of any error
     }
   }
 
@@ -60,6 +67,53 @@ class MongoDatabase {
     } catch (e) {
       print(e.toString());
       return "Sign Up Failed.";
+    }
+  }
+
+  static Future<List<Map<String, dynamic>>> getRequestDetailsByUser(
+      String userId) async {
+    try {
+      if (db == null) {
+        await connect();
+      }
+
+      var requests = await db!
+          .collection('maintenance_requests')
+          .find(where.eq('submittedBy', userId))
+          .toList();
+
+      return requests
+          .map((request) => request as Map<String, dynamic>)
+          .toList();
+    } catch (e) {
+      log(e.toString());
+      return [];
+    } finally {
+      if (db != null) {
+        await db!.close();
+      }
+    }
+  }
+
+  static Future<String?> getUserId(String email) async {
+    try {
+      final db = Db(
+          'mongodb+srv://facultymaintenance:fmms123@fmms.zwouah7.mongodb.net/?retryWrites=true&w=majority');
+      await db.open();
+
+      final userCollection = db.collection('users');
+      final user = await userCollection.findOne(where.eq('email', email));
+
+      await db.close();
+
+      if (user != null) {
+        return user['_id'].toString();
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Error retrieving user ID: $e');
+      return null;
     }
   }
 }
